a8cbdf592500db51bb48dbbd29426ed29d51a14d
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-02 18:25:38 -0300

    Se agrega funcion en caso de no encontrar usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { Container } from "@mui/material";
-import Searcher from "./components/Searcher";
-import { getGitHubUser } from './services/users'
-import UserCard from "./containers/UserCard";
+import React, { useEffect, useState } from 'react';
+import { Container, Typography } from '@mui/material';
+import Searcher from './components/Searcher';
+import { getGitHubUser } from './services/users';
+import UserCard from './containers/UserCard';
 
 const App = () => {
-
   const [inputUser, setInputUser] = useState('octocat');
-  const [userState, setUserState] = useState('inputUser');
+  const [userState, setUserState] = useState([]);
   const [notFound, setNotFound] = useState(false);
 
-  const gettinUser = async (user) => {
-    const userResponse = await getGitHubUser(user)
-
-    if(userState === 'octocat'){
-      localStorage.setItem('octocat', userResponse)
+  const gettingUser = async (user) => {
+    const userResponse = await getGitHubUser(user);
+    console.log('====================================');
+    console.log(userResponse);
+    console.log('====================================');
+    if (userState === 'octocat') {
+      localStorage.setItem('octocat', Json.stringify(userResponse));
+      setUserState(inputUser);
     }
 
-    if(userResponse.message === 'Not Found'){
-      const { octocat } = localStorage;
-      setInputUser(octocat);
+    if (userResponse.message === 'Not Found') {
       setNotFound(true);
+      const { octocat } = JSON.parse((localStorage.getItem('octocat')));
+      setInputUser(octocat);
     } else {
       setUserState(userResponse);
+      setNotFound(false);
     }
-
   };
 
   console.log(userState);
 
-  useEffect(() =>{
-    gettinUser(inputUser)
-  },
-  [inputUser])
+  useEffect(() => {
+    gettingUser(inputUser);
+  }, [inputUser]);
 
-  return(
-    <Container sx={{
-      background: 'whitesmoke',
-      width: '80vw',
-      height: '500px',
-      borderRadius: '16px',
-      marginTop: '40px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    }} >
-      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
-      <UserCard userState={userState}/>
+  return (
+    <Container
+      sx={{
+        background: 'whitesmoke',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Searcher setInputUser={setInputUser} />
+      {notFound ? (
+        <Typography sx={{ marginTop: '40px', color: 'red' }}>
+          User Not Found
+        </Typography>
+      ) : (
+        <UserCard userState={userState} />
+      )}
     </Container>
-  )
+  );
 };
 
 export default App;
