9a80cc4826aa13031d29c6ad64ac19184b44f92b
Leonardo Puebla <leonardo38361@gmail.com>
2022-11-02 14:57:27 -0300

    componente buscador

---
+++ src/components/Searcher/Searcher.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Stack, TextField, IconButton } from '@mui/material';
+
+const Searcher = () => {
+  return (
+    <Stack>
+      <TextField>
+        <IconButton></IconButton>
+      </TextField>
+    </Stack>
+  );
+};
+
+export default Searcher;
